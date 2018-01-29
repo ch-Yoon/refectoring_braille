f389181af0605973b75f75ed7423474c2f64b7f9
ch-Yoon <chanhyuck1021@naver.com>
2018-01-29 20:14:06 +0900

    accessibility info activity update

--- BrailleLearning/app/src/main/java/com/project/why/braillelearning/Accessibility/AccessibilityInfoActivity.java
+++ BrailleLearning/app/src/main/java/com/project/why/braillelearning/Accessibility/AccessibilityInfoActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.provider.Settings;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.TextView;
+import com.project.why.braillelearning.Global;
 import com.project.why.braillelearning.R;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,15 +20,15 @@ public class AccessibilityInfoActivity extends Activity {
     private TimerTask mTask;
     private Timer mTimer;
     private TextView guideTextView;
-
+    private int baseHeight = 0;
+    private int textSize = 5;
+    private boolean resizeStart = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accessibility_info);
-        guideTextView = (TextView) findViewById(R.id.accessibility_info_textview);
-        String guidText = getResources().getString(R.string.accessibility_guide);
-        guideTextView.setText(guidText.replace(" ", "\u00A0"));
 
+        guideTextView = (TextView) findViewById(R.id.accessibility_info_textview);
         guideTextView.setOnHoverListener(new View.OnHoverListener() {
             @Override
             public boolean onHover(View v, MotionEvent event) {
@@ -40,23 +44,97 @@ public class AccessibilityInfoActivity extends Activity {
                 return false;
             }
         });
-    }
 
+    }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) { // 화면에 포커스가 잡혔을 경우
             setFullScreen();
-            focusRequest();
+            setResizeText();
         }
+    }
+
+
+    /**
+     * 해상도에 반응하여 text view 안에 들어가는 글자 크기를 조절하는 함수
+     * 글자 크기를 textview의 크기에 꽉차게 조절 한 뒤, 2사이즈 작은 글자 크기로 설정
+     */
+    private void setResizeText(){
+        String guidText = getResources().getString(R.string.accessibility_guide);
+        guidText = guidText.replace(" ", "\u00A0");
+        guideTextView.setText(guidText);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    synchronized (this) {
+                        if(guideTextView != null) {
+                            if (resizeStart == false) {
+                                resizeStart = true;
+                                int height = guideTextView.getMeasuredHeight();
+                                baseHeight = height;
+
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        guideTextView.setTextSize(textSize);
+                                        guideTextView.setVisibility(View.INVISIBLE);
+                                        guideTextView.invalidate();
+                                        Log.d("dddd","aaaaaa");
+                                    }
+                                });
+
+                            } else {
+                                int height = guideTextView.getMeasuredHeight();
+                                if (height <= Global.DisplayY * 0.9) {
+                                    if (baseHeight != height) {
+                                        baseHeight = height;
+
+                                        runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                guideTextView.setTextSize(textSize);
+                                                guideTextView.invalidate();
+                                                textSize += 3;
+                                            }
+                                        });
+
+
+                                    }
+                                } else {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            textSize -= 5;
+                                            ViewGroup.LayoutParams params = guideTextView.getLayoutParams();
+                                            params.height = LinearLayoutCompat.LayoutParams.MATCH_PARENT;
+                                            guideTextView.setTextSize(textSize);
+                                            guideTextView.setVisibility(View.VISIBLE);
+                                            guideTextView.invalidate();
+                                            guideTextView.setLayoutParams(params);
+                                            focusRequest();
+                                        }
+                                    });
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        thread.start();
     }
 
     /**
      * 전체화면 함수
      * 소프트키는 숨기지 않는다.
      */
-    public void setFullScreen(){
+    private void setFullScreen(){
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
